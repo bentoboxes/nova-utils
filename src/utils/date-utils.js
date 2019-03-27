531a66d12240d1cbdef1f5e65470f62226a551d8
Alex Arriaga <alexzander.arriaga@base22.com>
2019-03-27 09:49:52 -0600

    Add formatDate() helpers

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -1,6 +1,25 @@
+import moment from 'moment';
+
 class DateUtils {
-  static formatDate() {
-    console.log('formatDate()');
+
+  static formatDate(date, outputFormat = 'DD/MM/YYYY', inputFormat = 'YYYY-MM-DDTHH:mm:ss') {
+    let dateParsed = '';
+
+    if (typeof date === 'number') {
+      date = parseInt(date);
+      dateParsed = moment(date); // Date in milliseconds
+    } else {
+      // Parse date using moment.js
+      if (typeof inputFormat !== 'undefined') {
+        dateParsed = moment(date, inputFormat);
+      } else {
+        dateParsed = moment(date);
+      }
+    }
+
+    return (dateParsed.isValid() && typeof outputFormat !== 'undefined') ?
+        dateParsed.format(outputFormat) // It uses the format() of moment.js
+        : date;
   }
 }
 
