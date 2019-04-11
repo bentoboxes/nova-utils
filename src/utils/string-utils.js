8b419c6bf5183c7f05746f01b88a66aa5070d1f2
Alex Arriaga <alexzander.arriaga@base22.com>
2019-04-11 09:23:46 -0500

    Add kebabToCamelCase and contains functions to String Utils

--- src/utils/string-utils.js
+++ src/utils/string-utils.js
@@ -34,6 +34,26 @@ class StringUtils {
       return '';
     }
   }
+
+  static capitalize(str) {
+    const splitStr = str.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(' ');
+  }
+
+  // TODO: Add to NovaUtils
+  static kebabToCamelCase(inputString) {
+    return inputString.replace(/(\-\w)/g, (m) => m[1].toUpperCase());
+  }
+
+  static contains(inputString, wordToFind) {
+    return inputString.indexOf(wordToFind) > -1;
+  };
 }
 
 export {StringUtils};
