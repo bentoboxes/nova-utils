782d470f3d7dbcf069ce62392e652804072ca82c
Alex Arriaga <alexzander.arriaga@base22.com>
2019-04-12 11:54:46 -0500

    Add initial test for WebBrowserUtils

---
+++ src/__tests__/web-browser-utils.spec.js
@@ -0,0 +1,10 @@
+/**
+ * @jest-environment jsdom
+ */
+import {WebBrowserUtils} from '../utils/web-browser-utils';
+
+test('it returns the version of Internet Explorer', () => {
+  const ieVersion = WebBrowserUtils.getInternetExplorerVersion();
+
+  expect(ieVersion).toEqual(-1);
+});
