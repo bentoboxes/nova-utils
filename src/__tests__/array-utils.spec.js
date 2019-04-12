33bdc36002caf418bba02e499e93357613ab3862
Alex Arriaga <alexzander.arriaga@base22.com>
2019-04-12 10:07:49 -0500

    Add coverage for array utils in case of moving items into a bigger array

--- src/__tests__/array-utils.spec.js
+++ src/__tests__/array-utils.spec.js
@@ -34,12 +34,26 @@ test('it gets unique elements in an array ', () => {
   expect(arrayWithUniqueElements).toEqual(expectedArray);
 });
 
-test('it moves an object in an array from one position (index) to other', () => {
-  const array = [{title: 'A'}, {title: 'B'}, {title: 'C'}];
-  const arrayMoved = [{title: 'B'}, {title: 'C'}, {title: 'A'}];
+test(
+    'it moves an object in an array from one position (oldIndex) to another (newIndex < array.length)',
+    () => {
+      const array = [{title: 'A'}, {title: 'B'}, {title: 'C'}];
+      const arrayMoved = [{title: 'B'}, {title: 'C'}, {title: 'A'}];
 
-  // Be aware this operation is mutable
-  ArrayUtils.moveItemInArray(array, 0, 2);
+      // Be aware this operation is mutable
+      ArrayUtils.moveItemInArray(array, 0, 2);
 
-  expect(array).toEqual(arrayMoved);
-});
+      expect(array).toEqual(arrayMoved);
+    });
+
+test(
+    'it moves and object in an array from one position (index) to another (newIndex >= array.length)',
+    () => {
+      const array = [{title: 'A'}, {title: 'B'}, {title: 'C'}];
+      const arrayMoved = [{title: 'B'}, {title: 'C'}, undefined, undefined, {title: 'A'}];
+
+      // Be aware this operation is mutable
+      ArrayUtils.moveItemInArray(array, 0, 4);
+
+      expect(array).toEqual(arrayMoved);
+    });
