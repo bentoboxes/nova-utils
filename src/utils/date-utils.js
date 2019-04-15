46ae0e82323acfb9b4929a154e77e8dfde4581f3
Eduardo Juarez <eduardo.juarez@base22.com>
2019-04-15 13:27:29 -0500

    Add utility to get relative dates

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -37,6 +37,41 @@ class DateUtils {
       ? dateParsed.format(outputFormat) // It uses the format() of moment.js
       : date;
   }
+
+  /**
+   * Returns a relative date using an start date, end date and input formats
+   * @static
+   * @param {string|number} startDate - The start date string
+   * @param {string|number} endDate - The end date string
+   * @param {string} startDateFormat='YYYY-MM-DD HH:mm:ss' - The format of the input string, it is used to parse the startDate parameter and validate it.
+   * @param {string} endDateFormat='YYYY-MM-DD HH:mm:ss' - The format of the input string, it is used to parse the endDate parameter and validate it.
+   * @retun {string} The relative date
+   */
+  static relativeDate(
+    startDate,
+    endDate,
+    startDateFormat = DEFAULT_INPUT_FORMAT,
+    endDateFormat = DEFAULT_INPUT_FORMAT
+    ) {
+      let startDateParsed = null;
+      let endDateParsed = null;
+
+      if (typeof startDate === "number" && typeof endDate === "number") {
+        startDate = parseInt(startDate);
+        endDate = parseInt(endDate);
+
+        startDateParsed = moment(startDate);
+        endDateParsed = moment(endDate);
+
+      } else {
+        startDateParsed = moment(startDate, startDateFormat);
+        endDateParsed = moment(startDate, endDateFormat);
+      }
+
+      return dateParsed.isValid()
+      ? moment(endDateParsed).from(startDateParsed, true)
+      : "from: " + startDate + " to: " + endDate;
+  }
 }
 
 export { DateUtils };
