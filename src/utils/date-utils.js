aabd36cfc42cce4dc880cb5d92314271bb87ead0
Eduardo Juarez <eduardo.juarez@base22.com>
2019-05-23 17:09:37 -0500

    Update date format mechanism

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -1,7 +1,17 @@
-import moment from "moment";
+import * as moment from "moment-timezone";
 
-const DEFAULT_INPUT_FORMAT = "YYYY-MM-DD HH:mm:ss";
+const INPUT_FORMATS = {
+  ISO: "YYYY-MM-DDTHH:mm:ss.sssZ",
+  SIMPLE_ISO: "YYYY-MM-DD HH:mm:ss",
+  MILLISECONDS: "x",
+  CALENDAR_DATE: "YYYY-MM-DD",
+  ORDINAL_DATE: "YYYY-DDD"
+};
+
+const DEFAULT_INPUT_FORMAT = INPUT_FORMATS.SIMPLE_ISO;
 const DEFAULT_OUTPUT_FORMAT = "DD/MM/YYYY";
+const DEFAULT_TIME_ZONE_INPUT = "America/Los_Angeles";
+const DEFAULT_TIME_ZONE_OUTPUT = "z";
 
 /** Class with static methods related to dates' management */
 class DateUtils {
@@ -11,29 +21,31 @@ class DateUtils {
    * @param {string|number} date - The date string to be formatted
    * @param {string} outputFormat='DD/MM/YYYY' - The desired output format
    * @param {string} inputFormat='YYYY-MM-DD HH:mm:ss' - The format of the input string, it is used to parse the date parameter
+   * @param {string} inputTimeZone='America/Los_Angeles' - The desired input time zone format
+   * @param {boolean} showTimeZone=true - Used to define if the time zone should be displayed
+   * @param {string} outputTimeZone='z' - The desired output time zone format
    * @retun {string} The formatted date
    */
   static formatDate(
     date,
     outputFormat = DEFAULT_OUTPUT_FORMAT,
-    inputFormat = DEFAULT_INPUT_FORMAT
+    inputFormat = DEFAULT_INPUT_FORMAT,
+    outputTimeZone = DEFAULT_TIME_ZONE_OUTPUT,
+    inputTimeZone = DEFAULT_TIME_ZONE_INPUT,
+    showTimeZone = true
   ) {
     let dateParsed = null;
 
-    if (typeof date === "number") {
-      dateParsed = moment(date); // Date in milliseconds
-    } else {
-      // Parse date using moment.js
-      // if (typeof inputFormat !== 'undefined') {
-      dateParsed = moment(date, inputFormat);
-      // }
-      // else {
-      //   dateParsed = moment(date);
-      // }
-    }
+    if (inputFormat !== INPUT_FORMATS.SIMPLE_ISO)
+      dateParsed = moment(date, INPUT_FORMATS[inputFormat]);
+    else dateParsed = moment(date, inputFormat);
+
+    dateParsed = moment.tz(dateParsed, inputTimeZone);
 
     return dateParsed.isValid()
-      ? dateParsed.format(outputFormat) // It uses the format() of moment.js
+      ? showTimeZone
+        ? dateParsed.format(`${outputFormat}, ${outputTimeZone}`)
+        : dateParsed.format(outputFormat) // It uses the format() of moment.js
       : date;
   }
 
@@ -45,7 +57,11 @@ class DateUtils {
    * @retun {string} The JavaScript Date object
    */
   static parseDate(date, inputFormat = DEFAULT_INPUT_FORMAT) {
-    const dateParsed = moment(date, inputFormat);
+    let dateParsed = null;
+
+    if (inputFormat !== INPUT_FORMATS.SIMPLE_ISO)
+      dateParsed = moment(date, INPUT_FORMATS[inputFormat]);
+    else dateParsed = moment(date, inputFormat);
 
     return dateParsed.isValid()
       ? dateParsed.toDate() // It uses the format() of moment.js
@@ -57,25 +73,19 @@ class DateUtils {
    * @static
    * @param {string|number} startDate - The start date string
    * @param {string|number} endDate - The end date string
-   * @param {string} startDateFormat='YYYY-MM-DD HH:mm:ss' - The format of the input string, it is used to parse the startDate parameter and validate it.
-   * @param {string} endDateFormat='YYYY-MM-DD HH:mm:ss' - The format of the input string, it is used to parse the endDate parameter and validate it.
+   * @param {string} inputFormat='YYYY-MM-DD HH:mm:ss' - The format of the input string, it is used to parse the startDate parameter and validate it.
    * @retun {string} The relative date
    */
-  static relativeDate(
-    startDate,
-    endDate,
-    startDateFormat = DEFAULT_INPUT_FORMAT,
-    endDateFormat = DEFAULT_INPUT_FORMAT
-  ) {
+  static relativeDate(startDate, endDate, inputFormat = DEFAULT_INPUT_FORMAT) {
     let startDateParsed = null;
     let endDateParsed = null;
 
-    if (typeof startDate === "number" && typeof endDate === "number") {
-      startDateParsed = moment(startDate);
-      endDateParsed = moment(endDate);
+    if (inputFormat !== INPUT_FORMATS.SIMPLE_ISO) {
+      startDateParsed = moment(startDate, INPUT_FORMATS[inputFormat]);
+      endDateParsed = moment(endDate, INPUT_FORMATS[inputFormat]);
     } else {
-      startDateParsed = moment(startDate, startDateFormat);
-      endDateParsed = moment(startDate, endDateFormat);
+      startDateParsed = moment(startDate, inputFormat);
+      endDateParsed = moment(endDate, inputFormat);
     }
 
     return startDateParsed.isValid() && endDateParsed.isValid()
