81e2d60e866a8760340ba4ef605716ac55846782
Alex Arriaga <alexzander.arriaga@base22.com>
2019-05-24 16:32:20 -0500

    Document new IBM portal utils

--- src/utils/wcm-utils.js
+++ src/utils/wcm-utils.js
@@ -73,7 +73,7 @@ class IBMWCMUtils {
    * @static
    * @param {string} youtubeURL - The Youtube URL the user could copy directly from a web browser
    * @return {string} a Youtube URL ready to be embedded in an iframe
-   * */
+   */
   static convertYoutubeUrlToBeEmbedded(youtubeURL) {
     if (typeof youtubeURL === "string") {
       const youtubeKey = youtubeURL.substring(
@@ -84,13 +84,23 @@ class IBMWCMUtils {
     return "#";
   }
 
-  static fixPortalURL(url) {
-    return url.replace(/&amp;/gi, "&");
+  /**
+   * Fix Portal's link URLs special characters such as "&amp;"
+   * @static
+   * @param {string} linkURL - The link URL to be fixed
+   * @return {string} a link URL without characters that prevent the redirection works as expected
+   */
+  static fixPortalLinkURL(linkURL) {
+    return linkURL.replace(/&amp;/gi, "&");
   }
 
+  /**
+   * Fix Portal's link URLs in a set of items (array) this can be used before sending the data.items
+   * to be rendered by a Nova component, e. g. data.items = NovaUtils.IBMWCMUtils.fixURLsInItems(data.items, "linkURL")
+   */
   static fixURLsInItems(items, urlField = "link") {
     items.forEach(item => {
-      item[urlField] = this.fixPortalURL(item[urlField]);
+      item[urlField] = this.fixPortalLinkURL(item[urlField]);
     });
     return items;
   }
