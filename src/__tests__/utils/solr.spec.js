8f1ec8b5b7ec4183b032742dd3e635301f3b5a6f
Alejandro Serrano <alexserrano63@gmail.com>
2020-01-25 19:43:41 -0600

    Add new fuctionalities to Solr API

--- src/__tests__/utils/solr.spec.js
+++ src/__tests__/utils/solr.spec.js
@@ -9,45 +9,50 @@ describe("Test new Solr instance", () => {
 });
 
 describe("Test query string", () => {
-  test("it has valid query", ()=> {
-    const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
-    const query = new Solr(baseUrl);
-    expect(query.baseUrl + query.buildQuery() + query.buildFilters()).toBe(baseUrl);
-  });
-
-  test("it has valid params", ()=> {
-    const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
-    const query = new Solr(baseUrl);
-    query.addQueryParams('authtemplate', 'news');
-
-    expect(query.buildQuery()).toBe('q=authtemplate:news');
-  })
-
-  test("it has valid query params one param", ()=> {
-    const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
-    const query = new Solr(baseUrl);
-    query.addQueryParams('authtemplate', 'news');
-    query.addToQuery(query.buildQuery());
-    expect(query.query).toBe('?q=authtemplate:news');
-  })
-
-  test("it has valid query params two params", ()=> {
-    const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
-    const query = new Solr(baseUrl);
-    query.addQueryParams('authtemplate', 'news');
-    query.addFilterParams('authtemplate', 'news');
-    query.addToQuery(query.buildQuery());
-    query.addToQuery(query.buildFilters());
-    expect(query.query).toBe('?q=authtemplate:news&fq=authtemplate:news');
-  });
-
-
   test("it has valid query extends", ()=> {
     const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
     const query = new Solr(baseUrl);
     query.q('authtemplate:news')
     expect(query.query).toBe('?q=authtemplate:news');
-  })
+  });
+
+  test("it has valid query extends adding and fq", ()=> {
+    const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
+    const query = new Solr(baseUrl);
+    query
+      .q('authtemplate:news')
+      .fq('authtemplate:news')
+
+    expect(query.query).toBe('?q=authtemplate:news&fq=authtemplate:news');
+  });
+
+  test("it has valid query extends adding and sort", ()=> {
+    const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
+    const query = new Solr(baseUrl);
+    query
+      .q('authtemplate:news')
+      .sort('news');
+    expect(query.query).toBe('?q=authtemplate:news&sort=news asc');
+  });
+
+  test("it has valid query extends adding and rows", ()=> {
+    const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
+    const query = new Solr(baseUrl);
+    query
+      .q('authtemplate:news')
+      .rows(100);
+    expect(query.query).toBe('?q=authtemplate:news&rows=100');
+  });
+
+  test("it has valid query extends adding and parse Object", ()=> {
+    const baseUrl = "/myservices/search-service/doRequest/dp-content/select";
+    const query = new Solr(baseUrl);
+    const parseObj = {date: "dateP"};
+    query
+      .q('authtemplate:news')
+      .addParseObject(parseObj);
+    expect(query.parseMap).toStrictEqual(parseObj);
+  });
 });
 
 describe("Test Solr utils", () => {
@@ -59,6 +64,11 @@ describe("Test Solr utils", () => {
   test("it has valid from now date ", ()=> {
     const date = Solr.dateFromNow();
     expect(date).toBe('[NOW/DAY TO *]');
+  });
+
+  test("it has valid and clause", ()=> {
+    const date = Solr.and("a", "b");
+    expect(date).toBe('(a AND b)');
   });
 
 });
--- src/utils/solr.js
+++ src/utils/solr.js
@@ -21,13 +21,103 @@ class Solr {
     this.query = "";
     this.parseMap = {};
     this.useJSON = false;
-    this.postQuery = {
-      query: "",
-      filter: "",
-      rows: "",
-      sort: ""
-    };
+    this.postQuery = {};
+    this.isQueryIncluded = false;
   };
+
+
+  /**
+   * @method and: The AND operator matches documents where both terms exist anywhere
+   * in the text of a single document.
+   * @param leftClause
+   * @param rightClause
+   * @returns {string}
+   */
+  static and(leftClause, rightClause) {
+    if (!this.prototype.__validString(leftClause) && !this.prototype.__validString(rightClause)) return "";
+
+    return `(${leftClause} AND ${rightClause})`;
+  }
+
+  /**
+   * @method or: The OR operator links two terms and finds a matching document if either
+   * of the terms exist in a document.
+   * @param leftClause
+   * @param rightClause
+   * @returns {string}
+   */
+  static or(leftClause, rightClause) {
+    if (!this.prototype.__validString(leftClause) && !this.prototype.__validString(rightClause)) return "";
+
+    return `(${leftClause} OR ${rightClause})`;
+  }
+
+  /**
+   * @method not: The NOT operator excludes documents that contain the term after NOT.
+   * @param {string} clause
+   * @returns {string}
+   */
+  static not(clause) {
+    if (!this.prototype.__validString(clause)) return "";
+
+    return `(! ${clause})`;
+  }
+
+  /**
+   * @method prohibit: Prohibits the following term (that is, matches on fields or documents
+   * that do not include that term).
+   * @param {string} clause
+   * @returns {string}
+   */
+  static prohibit(clause) {
+    if (!this.prototype.__validString(clause)) return "";
+
+    return `(- ${clause})`;
+  }
+
+  /**
+   * @method required: requires that the term after required symbol exist somewhere in a field
+   * in at least one document in order for the query to return a match.
+   * @param {string} clause
+   * @returns {string}
+   */
+  static required(clause) {
+    if (!this.prototype.__validString(clause)) return "";
+
+    return `(+ ${clause})`;
+  }
+
+  /**
+   * @method date: Add date value parsing the inputs to Solr valid format.
+   * @param {string} startDate
+   * @param {string} inputFormatDate | default: YYYY-MM-DD HH:mm:ss
+   * @param {string} finishDate | default: *
+   * @returns {string}
+   */
+  static date(startDate, inputFormatDate = INPUT_FORMATS.ISO, finishDate = "*") {
+    if (!this.prototype.__validString(startDate) && !this.prototype.__validString(finishDate)) return "";
+    startDate = DateUtils.formatCustomDate(startDate, SOLR_FORMAT_DATE, inputFormatDate);
+    if (finishDate !== "*") {
+      finishDate = DateUtils.formatCustomDate(finishDate, SOLR_FORMAT_DATE, inputFormatDate);
+    }
+
+    return `[${startDate}Z TO ${finishDate}]`;
+  }
+
+  /**
+   * @method dateFromNow: Add value to filter from today to an specific date or to any date.
+   * @param {string} finishDate | default: *
+   * @param {string} inputFormatDate | default: YYYY-MM-DD HH:mm:ss
+   * @returns {string}
+   */
+  static dateFromNow(finishDate = "*", inputFormatDate = INPUT_FORMATS.ISO) {
+    if (!this.prototype.__validString(finishDate)) return "";
+
+    if (finishDate !== "*") {
+      finishDate = DateUtils.formatCustomDate(finishDate, SOLR_FORMAT_DATE, inputFormatDate);
+    }
+    return `[NOW/DAY TO ${finishDate}]`;
+  }
 
 
   /**
@@ -39,182 +129,58 @@ class Solr {
     return this;
   }
 
-
   /**
-   * @param {String} paramName
-   * @param {String, Array} values
-   */
-  addQueryParams(paramName, values) {
-    this.queryParam[paramName] = values;
-  }
-
-  /**
-   * @static
-   * @param paramName
-   * @param values
-   */
-  addFilterParams(paramName, values) {
-    this.filterParams[paramName] = values;
-  }
-
-
-  /**
-   * @static
-   * @returns {string}
-   */
-  buildQuery() {
-    const queryParams = this.queryParam;
-    let queryString = Object.keys(queryParams).reduce((acc, curr) => {
-      let currentValues = queryParams[curr];
-      if (Array.isArray(currentValues)) {
-        acc += `${acc !== "" ? " AND " : ""}${curr}:${currentValues.join(",")}`;
-      } else {
-        acc += `${acc !== "" ? " AND " : ""}${curr}:${currentValues}`;
-      }
-      return acc;
-    }, "");
-
-    return queryString !== "" ? `q=${queryString}` : "";
-  }
-
-
-  /**
-   *
-   * @returns {string}
-   */
-
-  buildFilters() {
-    const filterParams = this.filterParams;
-    let filterString = Object.keys(filterParams).reduce((acc, curr) => {
-      let currentValues = filterParams[curr];
-      if (Array.isArray(currentValues)) {
-        acc += `${acc !== "" ? " AND " : ""}${curr}:${currentValues.join(",")}`;
-      } else {
-        acc += `${acc !== "" ? " AND " : ""}${curr}:${currentValues}`;
-      }
-      return acc;
-    }, "");
-
-    return filterString !== "" ? `fq=${filterString}` : "";
-  }
-
-
-  /**
-   *
-   * @param str
-   */
-  addToQuery(str) {
-    if (str !== "") {
-      this.query += this.query === "" ? `?${str}` : `&${str}`;
-    }
-  }
-
-
-  /**
-   *
+   * @method addParseKeyValue: Add a pair (key, value) with the equivalences
+   * that you expect to receive after the query.
    * @param key
    * @param value
    */
   addParseKeyValue(key, value) {
+    if (!this.__validString(key) && !this.__validString(value)) return this;
+
     this.parseMap[key] = value;
     return this;
   }
 
   /**
-   *
+   * @method addParseObject: Add a object with the equivalences that you expect
+   * to receive after the query.
    * @param obj
    */
-
   addParseObject(obj) {
+    if (!this.__validObj(obj)) return this;
     this.parseMap = Object.assign(this.parseMap, obj);
     return this;
   }
 
   /**
-   *
-   * @param leftClause
-   * @param rightClause
-   * @returns {string}
-   */
-  static and(leftClause, rightClause) {
-    return `(${leftClause} AND ${rightClause})`;
-  }
-
-  /**
-   *
-   * @param leftClause
-   * @param rightClause
-   * @returns {string}
-   */
-  static or(leftClause, rightClause) {
-    return `(${leftClause} OR ${rightClause})`;
-  }
-
-  /**
-   *
-   * @param clause
-   * @returns {string}
-   */
-  static not(clause) {
-    return `(! ${clause})`;
-  }
-
-  /**
-   *
-   * @param clause
-   * @returns {string}
-   */
-  static prohibit(clause) {
-    return `(- ${clause})`;
-  }
-
-  /**
-   *
-   * @param {string} startDate
-   * @param {string} inputFormatDate | default: YYYY-MM-DD HH:mm:ss
-   * @param {string} finishDate | default: *
-   * @returns {string}
-   */
-  static date(startDate, inputFormatDate = INPUT_FORMATS.ISO, finishDate = "*") {
-
-    startDate = DateUtils.formatCustomDate(startDate, SOLR_FORMAT_DATE, inputFormatDate);
-    if (finishDate !== "*") {
-      finishDate = DateUtils.formatCustomDate(finishDate, SOLR_FORMAT_DATE, inputFormatDate);
-    }
-
-    return `[${startDate}Z TO ${finishDate}]`;
-  }
-
-  /**
-   *
-   * @param {string} finishDate | default: *
-   * @param {string} inputFormatDate | default: YYYY-MM-DD HH:mm:ss
-   * @returns {string}
-   */
-  static dateFromNow(finishDate = "*", inputFormatDate = INPUT_FORMATS.ISO) {
-    if (finishDate !== "*") {
-      finishDate = DateUtils.formatCustomDate(finishDate, SOLR_FORMAT_DATE, inputFormatDate);
-    }
-    return `[NOW/DAY TO ${finishDate}]`;
-  }
-
-  /**
-   *
+   * @method fq: fq is used to reduce the superset of documents that can be returned.
    * @param params
    * @returns {Solr}
    */
   fq(params) {
-    this.query += this.__addQueryParam(params ? `fq=${params}` : "");
+    if (!this.__validString(params)) return this;
+    if (this.useJSON) {
+      this.postQuery.filter.push(params);
+    } else {
+      this.query += this.__addQueryParam(params ? `fq=${params}` : "");
+    }
     return this;
   }
 
   /**
-   *
+   * @method q: q is used to restrict Solr query.
    * @param params
    * @returns {Solr}
    */
   q(params) {
-    this.query += this.__addQueryParam(params ? `q=${params}` : "");
+    if (!this.__validString(params)) return this;
+    if (this.useJSON) {
+      this.postQuery.query.push(params);
+    } else {
+      this.query += this.__addQueryParam(params ? `q=${params}` : "");
+    }
+    this.isQueryIncluded = true;
     return this;
   }
 
@@ -225,29 +191,69 @@ class Solr {
    * @returns {Solr}
    */
   fl(elements) {
-    if (Array.isArray(elements) && elements.length) {
-      this.query += this.__addQueryParam(`fl=${elements.join(",")}`);
+    if (this.useJSON) {
+      this.postQuery.fields = elements;
+    } else {
+      if (Array.isArray(elements) && elements.length) {
+        this.query += this.__addQueryParam(`fl=${elements.join(",")}`);
+      }
     }
     return this;
   }
 
   /**
-   *
+   * @method sort: Add sort parameter to query.
    * @param paramName
    * @param asc
    * @returns {Solr}
    */
   sort(paramName, asc = true) {
-    this.query += this.__addQueryParam(`${paramName} ${asc ? SORT["ASC"] : SORT["DESC"]}`);
+    if (this.useJSON) {
+      this.postQuery.sort = `${paramName} ${asc ? SORT["ASC"] : SORT["DESC"]}`;
+    } else {
+      this.query += this.__addQueryParam(`sort=${paramName} ${asc ? SORT["ASC"] : SORT["DESC"]}`);
+    }
     return this;
   }
 
   /**
-   *
+   * @method: Add start number to return documents from that index.
+   * @param {number} start
+   * @returns {Solr}
+   */
+  start(start = 0) {
+    if (this.useJSON) {
+      this.postQuery.start = start;
+    } else {
+      this.query += this.__addQueryParam(`start=${start}`);
+    }
+    return this;
+  }
+
+  /**
+   * @method: Add end number to return documents until that index.
+   * @param {number} limit
+   * @returns {Solr}
+   */
+  rows(limit) {
+    if (this.useJSON) {
+      this.postQuery.limit = limit;
+    } else {
+      this.query += this.__addQueryParam(`rows=${limit}`);
+    }
+    return this;
+  }
+
+  /**
+   * @method execute: this function make an ajax call to Solr and retrieve the response.
    * @returns {Promise|*|Promise<T | Array>|*}
    */
   execute() {
-    return HttpClient.get(this.baseUrl + this.query)
+    let query = this.useJSON ?
+      HttpClient.post(this.baseUrl, this.postQuery) :
+      HttpClient.get(this.baseUrl + this.query);
+
+    return query
       .then((response) => response.json())
       .then(jsonResponse => {
         return this.__parseData(jsonResponse);
@@ -280,7 +286,13 @@ class Solr {
         response = response.reduce((acc, curr) => {
           let obj = {};
           keys.forEach(key => {
-            obj[this.parseMap[key]] = curr[key];
+            if (Array.isArray(curr[key])) {
+              for (let item of curr[key]) {
+                obj[item] = curr[key];
+              }
+            } else {
+              obj[this.parseMap[key]] = curr[key];
+            }
           });
           acc.push(obj);
 
@@ -306,6 +318,25 @@ class Solr {
     return "";
   }
 
+  /**
+   * @method __validString: validation to avoid add empty strings
+   * @param input
+   * @returns {boolean}
+   * @private
+   */
+  __validString(input) {
+    return typeof input === "string" && input.trim() !== "";
+  }
+
+  /**
+   * @method __validObj: validation to avoid empty objects.
+   * @param obj
+   * @private
+   */
+  __validObj(obj){
+    const keys = Object.keys(obj);
+    return typeof obj === "object" && keys.length && !keys.some(k => !this.__validString(obj[k]));
+  }
 }
 
 export { Solr };
